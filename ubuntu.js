4d95b984d4dd86126c4e1d8f7ae249ef5bb9364e
Kimpe Andy <andykimpe@gmail.com>
2024-02-13 18:08:04 +0100

    Update ubuntu.js

--- ubuntu.js
+++ ubuntu.js
@@ -11,6 +11,7 @@
 		firefox = document.querySelector('input[name=firefox]'),
 		apache2 = document.querySelector('input[name=apache2]'),
 		php = document.querySelector('input[name=php]'),
+		nginx = document.querySelector('input[name=nginx]'),
 		security = document.querySelector('input[name=security]');
 
 	var sourceList = [];
@@ -85,6 +86,11 @@
 		if(php.checked) {
 			appendSource(['sudo add-apt-repository ppa:ondrej/php -y']);
 			if(src.checked) appendSource(['sudo add-apt-repository ppa:ondrej/php -y -s']);
+		}
+		if(nginx.checked) {
+			appendSource(['wget -q https://nginx.org/keys/nginx_signing.key -O- | sudo tee /usr/share/keyrings/nginx-archive-keyring.gpg > /dev/null']);
+			appendSource(['echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg]', arch, 'http://nginx.org/packages/ubuntu/', rel, 'main'+ '" | sudo tee -a /etc/apt/sources.list.d/nginx.list > /dev/null']);
+			if(src.checked) appendSource(['echo "deb-src [signed-by=/etc/apt/keyrings/php.asc]', arch, 'http://nginx.org/packages/ubuntu/', rel, 'main'+ '" | sudo tee -a /etc/apt/sources.list.d/nginx.list > /dev/null']);
 		}
 
 
