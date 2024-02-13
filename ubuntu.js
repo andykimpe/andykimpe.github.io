0af575bdb7ed03d1be8bc4d2ac1d4ea38e51e2a3
Kimpe Andy <andykimpe@gmail.com>
2024-02-13 17:29:09 +0100

    Update ubuntu.js

--- ubuntu.js
+++ ubuntu.js
@@ -66,7 +66,7 @@
 			appendSource(['sudo apt-get -y purge firefox']);
 			appendSource(['sudo install -d -m 0755 /etc/apt/keyrings']);
 			appendSource(['wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null']);
-			appendSource(['echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc]', arch, 'https://packages.mozilla.org/apt', 'mozilla', 'main', '" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null']);
+			appendSource(['echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc]', arch, 'https://packages.mozilla.org/apt', 'mozilla', 'main'+ '" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null']);
 			appendSource(['echo "']);
 			appendSource(['Package: *']);
 			appendSource(['Pin: origin packages.mozilla.org']);
